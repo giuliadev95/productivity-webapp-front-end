a3b4a1a0cdce3008c59d1adb24152d15142077a9
giulia <giuliamoukouyou@gmail.com>
2026-01-19 21:10:38 +0100

    feat: add function to add a new todo list from the user input

--- src/App.tsx
+++ src/App.tsx
@@ -9,17 +9,36 @@ import { createTodo, type Todo } from "./todos";
 
 function App() {
   const [todos, setTodos] = useState<Todo[]>([]);
+  const [input, setInput] = useState<string>("");
+  const [error, setError] = useState<boolean>(false);
   function addTodo() {
-    setTodos((prev) => [...prev, createTodo("mock todo")]);
+    if (input == "") {
+      setError(true);
+      return;
+    }
+    setError(false);
+    setTodos((prev) => [...prev, createTodo(input)]);
+    setInput("");
   }
   return (
     <>
       <h1>My tasks</h1>
-
-      <button type="button" onClick={addTodo}>
-        +
-      </button>
-
+      <label htmlFor="add-task">Add task</label>
+      <div className="flex">
+        <input
+          id="add-task"
+          placeholder="Add a new task"
+          type="text"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+        <button type="button" onClick={addTodo}>
+          +
+        </button>
+      </div>
+      {error && (
+        <span className="text-red-700">You can't add an empty task!</span>
+      )}
       <TodoList todoList={todos} />
     </>
   );
